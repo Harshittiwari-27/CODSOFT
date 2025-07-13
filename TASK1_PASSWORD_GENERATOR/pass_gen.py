2dbef914fa94892793092e2928253f6a3e130627
Harshittiwari-27 <027harshittiwari@gmail.com>
2025-07-14 04:08:05 +0530

    Completed logic with edge case handling

--- TASK1_PASSWORD_GENERATOR/pass_gen.py
+++ TASK1_PASSWORD_GENERATOR/pass_gen.py
@@ -1,7 +1,25 @@
 import random
 import string
-msg=int(input("Enter the length password u want to generate :"))
-# print(string.digits[:msg])
+msg=input("Enter the length password u want to generate :")
 
-gen_no=random.choices(string.digits,k=msg)
-print("".join(gen_no))
+# check that  password length must not be alphabets
+
+if msg.isalpha():
+    print("Enter password length only in digits")
+
+
+elif not msg.isdigit():
+    print("Only numbers are allowed for password length.")
+
+
+
+else:
+    msg_int = int(msg)          #   -----------      converting input that we have taken into integer
+
+    if msg_int <= 0:
+        print("Password length must be positive ")
+ 
+    
+
+    gen_no=random.choices(string.digits+string.ascii_lowercase+string.ascii_uppercase+string.punctuation,k=msg_int)
+    print("Generated password are\n"+"".join(gen_no))
