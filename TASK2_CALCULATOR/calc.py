f17de1c0e0b77a439f95d1725610cec709954131
Harshittiwari-27 <027harshittiwari@gmail.com>
2025-07-15 12:09:03 +0530

    Task 2: Completed calculator with menu choices,operations and most imp error handling also taken

--- TASK2_CALCULATOR/calc.py
+++ TASK2_CALCULATOR/calc.py
@@ -1,15 +1,22 @@
 #                                         Calculator logic and everthing will be here :-
-num2=float(input("Enter the first no :\n"))
-num1=float(input("Enter the second no :\n"))
+try:
+       
+    num1=float(input("Enter the first no :\n"))
+    num2=float(input("Enter the second no :\n"))
+except ValueError:                          # If user input other than number it will throw  ERROR !!
+       print(" ERROR !!  Please enter the numbers only :")
+       exit()
+       
+
 
 #                                          Menu for operation choices :-
-print("\n--- Calculator ---")
+print("\n--- Simple Calculator ---")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
 
-choice = input("Enter your choice (1/2/3/4): ")
+choice = input("Enter your choice to do perform the above operation that is  (1/2/3/4): ")
 
 #                                          Opreations that to be performed :-
 
@@ -24,9 +31,11 @@ elif choice == '3':
         print(f"Result: {num1} * {num2} = {result}")
 elif choice == '4':
         if num2 == 0:
-            print("Error: Cannot divide by zero.")
+            print("Error !!  Cannot divide by zero.")
         else:
             result = num1 / num2
             print(f"Result: {num1} / {num2} = {result}")
 else:
         print("Invalid choice. Please select only from 1, 2, 3, or 4.")
+
+# YOUR OUTPUT WILL BE DISPLAYED ON THE SCREEN :-
